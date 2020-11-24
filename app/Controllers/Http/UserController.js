143043c7181a3402e8bd8382a1a740e9cebf38c8
exlleysantos <exlleyclemente@gmail.com>
2020-11-24 20:48:36 -0300

    CRUD User

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -4,10 +4,30 @@ const User = use("App/Models/User");
 
 class UserController {
     async store ({ request }) {
-        const data = request.only(["username", "email", "password", "cpf", "avatar", "lattes"]);
+        const data = request.only(["username", "email", "password", "cpf"]);
         
         return await User.create(data);
-      }
+    }
+
+    async index (){
+        
+        return User.all();
+    }
+
+    async show ({ params }) {
+    
+        return await User.findOrFail(params.id);
+    }
+
+    async destroy ({ params, auth, response }) {
+        const user = await User.findOrFail(params.id);
+      
+        if (user.id !== auth.user.id) {
+          return response.status(401).send({ error: 'Not authorized' });
+        }
+    
+        await user.delete();
+    }
 }
 
 module.exports = UserController;
